387cefe572d6300edee02bbe00f969351e8eccd4
kehiy <kehiiiiya@gmail.com>
2023-06-30 09:33:23 -0500

    refactor: done

--- meteoroid.py
+++ meteoroid.py
@@ -1,52 +1,11 @@
 
 import pygame , time , random
-#---------
+
+import src.variables as vars
+
 pygame.init()
-
-crash_sound = pygame.mixer.Sound("./public/lose-m.wav")
-pygame.mixer.music.load("./public/world-m.ogg")
-
-display_width = 800
-display_height = 600
-
-bg = pygame.image.load('./public/bg.png')
-
-black = (0,0,0)
-white = (255,255,255)
-red = (255,0,0)
-
-
-display = pygame.display.set_mode((display_width , display_height))
-pygame.display.set_caption('./public/Meteoroid v-1.6.9')
-icon = pygame.image.load('./public/icon1.png')
-pygame.display.set_icon(icon)
-
-clock = pygame.time.Clock()
-
-
-space_craft = pygame.image.load('./public/space_craft.png')
-craft_width = 48
-meteor_obj = pygame.image.load('./public/meteor.png')
-
-    
-def button(msg,x,y,w,h,inac,ac,action=None):
-    mouse = pygame.mouse.get_pos()
-    click = pygame.mouse.get_pressed()
-
-    pygame.draw.rect(display , ac , (x,y,w,h))
-    if x + w > mouse[0] > x and y + h > mouse[1] > y:
-        pygame.draw.rect(display , inac , (x,y,w,h))
-        if click[0] == True and action != None:
-            if action == "play":
-                game_loop()
-            elif action == "Quit":
-                pygame.quit()
-                
-            
-    button_text = pygame.font.Font("./public/FreeSansBold.ttf" , 20)
-    textsurf , textrect =  text_object(msg , button_text)
-    textrect.center = ((x + (w/2)) , (y + (h/2)))
-    display.blit(textsurf , textrect)
+pygame.display.set_icon(vars.icon)
+pygame.display.set_caption('Meteoroid v2')
 
 def start():
     intro = True
@@ -57,9 +16,9 @@ def start():
                 quit()
         largetext = pygame.font.SysFont(None , 60)
         textsurf , textrect = text_object("lets go explore space!" , largetext)
-        textrect.center = ((display_width/2),(150))
-        display.blit(bg, (0, 0))
-        display.blit(textsurf , textrect)
+        textrect.center = ((vars.display_width/2),(150))
+        vars.display.blit(vars.bg, (0, 0))
+        vars.display.blit(textsurf , textrect)
 
 
         button("GO!" , 150 , 450 , 100 , 50 , (250,100,100) , (100,250,0) , "play")
@@ -68,40 +27,59 @@ def start():
 
         pygame.display.update()
 
-
 def score(count):
     font = pygame.font.SysFont(None , 25)
     text = font.render("dodged meteors:" +str(count) , True , (100,100,255))
-    display.blit(text , (0,0))
+    vars.display.blit(text , (0,0))
 
 def meteor(meteorx , meteory , meteorw , meteorh):
-    display.blit(meteor_obj , [meteorx,meteory,meteorw,meteorh])
+    vars.display.blit(vars.meteor_obj , [meteorx,meteory,meteorw,meteorh])
 
 def craft(x,y):
-    display.blit(space_craft , (x,y))
+    vars.display.blit(vars.space_craft , (x,y))
 
 def text_object(text , font):
-    textsurface = font.render(text , True , red)
+    textsurface = font.render(text , True , vars.red)
     return textsurface , textsurface.get_rect()
 
 def message_display(text):
     largetext = pygame.font.Font('freesansbold.ttf' , 60)
     textsurf , textrect = text_object(text , largetext)
-    textrect.center = ((display_width/2),(display_height/2))
-    display.blit(textsurf , textrect)
+    textrect.center = ((vars.display_width/2),(vars.display_height/2))
+    vars.display.blit(textsurf , textrect)
 
     pygame.display.update()
 
     time.sleep(2)
     game_loop()
 
+def button(msg,x,y,w,h,inac,ac,action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+
+    pygame.draw.rect(vars.display , ac , (x,y,w,h))
+    if x + w > mouse[0] > x and y + h > mouse[1] > y:
+        pygame.draw.rect(vars.display , inac , (x,y,w,h))
+        if click[0] == True and action != None:
+            if action == "play":
+                game_loop()
+            elif action == "Quit":
+                pygame.quit()
+                
+            
+    button_text = pygame.font.Font("./public/FreeSansBold.ttf" , 20)
+    textsurf , textrect =  text_object(msg , button_text)
+    textrect.center = ((x + (w/2)) , (y + (h/2)))
+    vars.display.blit(textsurf , textrect)
+
+
 def crash():
     pygame.mixer.music.stop()
-    pygame.mixer.Sound.play(crash_sound)
+    pygame.mixer.Sound.play(vars.crash_sound)
     largetext = pygame.font.Font('freesansbold.ttf' , 60)
     textsurf , textrect = text_object("space craft exploded!" , largetext)
-    textrect.center = ((display_width/2),(display_height/2))
-    display.blit(textsurf , textrect)
+    textrect.center = ((vars.display_width/2),(vars.display_height/2))
+    vars.display.blit(textsurf , textrect)
 
     while 1:
         for events in pygame.event.get():
@@ -113,17 +91,15 @@ def crash():
 
         pygame.display.update()
 
-    
-
 def game_loop():
 
     pygame.mixer.music.play(-1)
 
-    x = (display_width * 0.45)
-    y = (display_height * 0.8)
+    x = (vars.display_width * 0.45)
+    y = (vars.display_height * 0.8)
     x_ch = 0
 
-    meteor_sx = random.randrange(0,display_width)
+    meteor_sx = random.randrange(0,vars.display_width)
     meteor_sy = -600
     meteor_speed = 7
     meteor_width = 100
@@ -150,7 +126,7 @@ def game_loop():
 
         x += x_ch
 
-        display.blit(bg, (0, 0))
+        vars.display.blit(vars.bg, (0, 0))
         
         meteor(meteor_sx , meteor_sy , meteor_width , meteor_height)
         meteor_sy += meteor_speed
@@ -158,12 +134,12 @@ def game_loop():
         score(score_count)
 
         craft(x,y)     
-        if x > display_width - craft_width or x < 0:
+        if x > vars.display_width - vars.craft_width or x < 0:
             crash()
         
-        if meteor_sy > display_height:
+        if meteor_sy > vars.display_height:
             meteor_sy = 0 - meteor_height
-            meteor_sx = random.randrange(0 , display_width)
+            meteor_sx = random.randrange(0 , vars.display_width)
             score_count += 1
             
             if (score_count % 7 == 0 ):
@@ -171,15 +147,15 @@ def game_loop():
 
         
         if y < meteor_sy + meteor_height:
-            if x > meteor_sx and x < meteor_sx + meteor_height or x + craft_width > meteor_sx and x + craft_width < meteor_sx + meteor_width:
+            if x > meteor_sx and x < meteor_sx + meteor_height or x + vars.craft_width > meteor_sx and x + vars.craft_width < meteor_sx + meteor_width:
                 crash()
 
 
         pygame.display.update()
-        clock.tick(60)
-    
-   
-start()
-game_loop()
-pygame.quit()
-quit() 
+        vars.clock.tick(60)
+
+if __name__ == "__main__":
+    start()
+    game_loop()
+    pygame.quit()
+    quit() 
